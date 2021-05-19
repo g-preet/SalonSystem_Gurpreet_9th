f97fd7f5ea1eb3a52a1f97a123bf9ab6ae3b7b92
Gopisandhu77@icloud.com <Gopisandhu77@icloud.com>
2021-05-19 23:10:19 +0300

    Add project files.

---
+++ SalonSystem_Gurpreet/Controllers/StaffAPIController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalonSystem_Gurpreet.Controllers
+{
+    public class StaffAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SalonSystem_Gurpreet/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SalonSystem_Gurpreet.Models;
+
+namespace SalonSystem_Gurpreet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
